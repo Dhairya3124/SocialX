4868be1b657a856b47d1e971f0d0e232a339fff6
Dhairya Khanna <dk9582671597@gmail.com>
2024-12-10 22:09:58 +0530

    feat: add .env settings

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,13 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/Dhairya3124/SocialX/internal/env"
+)
 
 func main() {
-	cfg := config{addr: ":8080"}
+	cfg := config{addr: env.GetString("ADDR",":8080")}
 	app := &application{
 		config: cfg,
 	}
