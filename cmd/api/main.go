f3a963997f5d71df14c3562e45844a3613b8ee93
Dhairya Khanna <dk9582671597@gmail.com>
2024-12-11 21:32:23 +0530

    feat: add store connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,12 +4,15 @@ import (
 	"log"
 
 	"github.com/Dhairya3124/SocialX/internal/env"
+	"github.com/Dhairya3124/SocialX/internal/store"
 )
 
 func main() {
 	cfg := config{addr: env.GetString("ADDR",":8080")}
+	store:=store.NewStorage(nil)
 	app := &application{
 		config: cfg,
+		store: store,
 	}
 	mux := app.mount()
 	log.Fatal(app.run(mux))
