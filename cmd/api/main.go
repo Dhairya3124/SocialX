fc3b98d456e8f3fb925b2c0672150e39b7fd2adc
Dhairya Khanna <dk9582671597@gmail.com>
2024-12-14 18:26:03 +0530

    feat: added db connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,13 +3,28 @@ package main
 import (
 	"log"
 
+	"github.com/Dhairya3124/SocialX/internal/db"
 	"github.com/Dhairya3124/SocialX/internal/env"
 	"github.com/Dhairya3124/SocialX/internal/store"
 )
 
 func main() {
-	cfg := config{addr: env.GetString("ADDR",":8080")}
-	store:=store.NewStorage(nil)
+	cfg := config{
+		addr: env.GetString("ADDR",":8080"),
+		db: dbConfig{
+			addr:env.GetString("DB_ADDR","postgres://user:adminpassword@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS",30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS",30),
+			maxIdleTime: env.GetString("DB_MAX_IDLE_TIME","15min"),
+		},
+	
+	}
+	db,err:=db.New(cfg.db.addr,cfg.db.maxOpenConns,cfg.db.maxIdleConns,cfg.db.maxIdleTime)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	store:=store.NewStorage(db)
 	app := &application{
 		config: cfg,
 		store: store,
